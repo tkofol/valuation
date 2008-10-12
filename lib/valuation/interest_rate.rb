fdc08b6131b1c3a022d84a919275f9c3cbc7e7bc
Tim Kofol <tkofol@gmail.com>
2008-10-12 13:35:33 -0400

    Added more interest rate time periods

--- lib/valuation/interest_rate.rb
+++ lib/valuation/interest_rate.rb
@@ -1,9 +1,11 @@
+include Math
 module Valuation
   
   class InterestRate
     attr_accessor :rate
-    @@periods = {:yearly => 1,
-                 :bi_annualy => (1/2.0),
+    @@periods = {:yearly => 1.0,
+                 :semiannually => (1/2.0),
+                 :bi_annually => (1/2.0),
                  :quarterly => (1/4.0),
                  :monthly => (1/12.0),
                  :weekly => (1/52.0),
@@ -16,6 +18,15 @@ module Valuation
       end
     end
     
+    # Add a unique time period. If you need different time
+    # periods that are not provided by default
+    # 
+    # All time periods are measured to a base time period of 1 year
+    # so to add an hourly time period
+    #
+    # InterestRate.add_time_periods(:hourly => (1/(365*24)))
+    # This time period will now be available, in the init method.
+    # It will also add a instance method hourly
     def self.add_time_periods(periods)
       @@periods.merge(periods)
       periods.each_pair do |period, n| 
@@ -25,13 +36,36 @@ module Valuation
       end
     end
      
-    def initialize(rate, options = {})
-      @rate = rate
+    #  All interest rates are converted to EAR rates.
+    #  So the base time period is one year. 
+    #  
+    #  10% EAR rate: InterestRate.new(0.10)
+    #  
+    #  5% EMR rate: InterestRate.new(0.05, :monthly)
+    #
+    #  12% APR compounded monthly InterestRate.new(0.12, :apr, :compounded => :monthly)
+    #
+    #  Custom Period 5% Compounded every third of the year InterestRate.new(0.05, 1/3.0)
+    def initialize(rate, type=:yearly, options = {})
+      if type.class.to_s == 'Float'
+        @rate = convert_period(1/type, rate)
+      elsif type.to_sym == :apr
+        options = {:compounded => :yearly}.merge(options)
+        factor = @@periods[options[:compounded]]
+        @rate = (1 + (rate*factor.to_f))**(1/factor.to_f) - 1
+      else
+        @rate = convert_period(1/@@periods[type], rate)
+      end
+    end
+    
+    # The continous interest rate for a given time period t
+    def continuous(t)
+      E ** (rate * t) - 1
     end
     
     private
-      def convert_period(n)
-        ((1 + @rate) ** n) - 1
+      def convert_period(n, rate=@rate)
+        ((1 + rate) ** n) - 1
       end 
   end
   
--- test/test_interest_rate.rb
+++ test/test_interest_rate.rb
@@ -10,7 +10,7 @@ class TestInterestRate < Test::Unit::TestCase
   
   def test_bi_annually
     rate = InterestRate.new(0.10)
-    assert_in_delta 0.0488088481701, rate.bi_annualy, 0.0001
+    assert_in_delta 0.0488088481701, rate.bi_annually, 0.0001
   end
   
   def test_monthly
@@ -24,5 +24,26 @@ class TestInterestRate < Test::Unit::TestCase
     assert_not_nil rate.once_in_a_blue_moon
     rate2 = InterestRate.new(0.10)
     assert_not_nil rate.once_in_a_blue_moon
+  end
+  
+  def test_apr_monthly
+    rate = InterestRate.new(0.12, :apr, :compounded => :monthly)
+    assert_in_delta 0.01, rate.monthly, 0.0001
+    assert_in_delta 0.12682503013197, rate.rate, 0.0001
+  end
+  
+  def test_apr_semi_annually
+    rate = InterestRate.new(0.12, :apr, :compounded => :semiannually)
+    assert_in_delta 0.06, rate.semiannually, 0.0001
+  end
+  
+  def test_continuous_interest_rate
+    rate = InterestRate.new(0.05)
+    assert_in_delta 0.28402, rate.continuous(5), 0.0001
+  end
+  
+  def test_custom_interest_rate
+    rate = InterestRate.new(0.05, (1/3.0))
+    assert_in_delta 0.157625, rate.rate, 0.0001
   end
 end
